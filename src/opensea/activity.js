df73dcf4f28cb452aa98cedc791013d47bff02ad
Kipras Januska <kiprasjanuska0@gmail.com>
2021-10-05 12:58:25 -0400

    Add instructions on how to get nft price

--- src/opensea/activity.js
+++ src/opensea/activity.js
@@ -2,6 +2,19 @@ const axios = require('axios')
 
 // https://docs.opensea.io/reference/retrieving-asset-events
 
+/*
+GET COST OF MINTING COLLECTION
+
+1. use opensea api to get collection and the contract address of each nft
+in the collection
+
+2. crawl through transaction list from etherscan and check if contract addy
+in transaction matches the addy from opensea
+
+3. if yes, check if not error. then, 'value' is the mint price (if not 0)
+and the gas price is 'gasPrice' * 'gasUsed' divided by something like 10^17
+*/
+
 async function getActivity(address, event_type=null) {
   try {
     var resp = await axios.get(
